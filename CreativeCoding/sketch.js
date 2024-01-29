0bb2b62037ab29fd3e68dcad6463149179ea0dae
summitmock <summitmock@gmail.com>
2024-01-29 08:17:08 -0500

    new

---
+++ CreativeCoding/sketch.js
@@ -0,0 +1,35 @@
+let gif;
+
+function preload (){
+gif = loadImage ('assets/DOTS.gif');
+
+}
+
+
+function setup() {// runs one time
+  createCanvas(windowWidth, windowHeight);
+    background('#fffec8');
+  strokeWeight(1);
+  fill('#fffd8d')
+}
+
+function draw() {//runs in a loop
+  background ('#fffec8');
+  var num = 10;
+  var sideLen = windowWidth/num;
+  for (var y = 0; y < windowHeight; y = y + sideLen) {//loop to create a grid in the y direction
+    for (var x = 0; x < windowWidth; x = x + sideLen) {
+      quad (x,y,x + sideLen, y, x +sideLen, y + sideLen,x, y + sideLen);
+      
+       }
+  }
+}
+      
+  function windowResized(){
+    resizeCanvas (windowWidth,windowHeight);
+  
+
+      
+ 
+  
+  }
