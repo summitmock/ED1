062d40e68821e528a1d37123a7f0719d8428eddd
summitmock <summitmock@gmail.com>
2024-01-30 08:21:58 -0500

    update

--- sketch.js
+++ sketch.js
@@ -8,9 +8,10 @@ gif = loadImage ('assets/summit.gif');
 
 function setup() {// runs one time
   createCanvas(windowWidth, windowHeight);
-    background('#fffec8');
-  strokeWeight(1);
-  fill('#fffd8d')
+    fill('#fffd8d');
+    stroke ('white');
+    strokeWeight(1);
+
 }
 
 function draw() {//runs in a loop
@@ -19,7 +20,7 @@ function draw() {//runs in a loop
   var sideLen = windowWidth/num;
   for (var y = 0; y < 2 * windowHeight; y = y + sideLen) {//loop to create a grid in the y direction
     for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
-      quad (x,y,x + sideLen, y, x +sideLen, y + sideLen,x, y + sideLen);
+      
       
        }
   }
