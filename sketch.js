dcf4fb2ae5eccf5816e4c96b4ef103ee80b96b4d
summitmock <summitmock@gmail.com>
2024-01-30 09:03:46 -0500

    push

--- sketch.js
+++ sketch.js
@@ -16,8 +16,10 @@ function setup() {// runs one time
 
 function draw() {//runs in a loop
   background ('#fffec8');
+  
   var num = 10;
   var sideLen = windowWidth/num;
+  
   for (var y = 0; y < 2 * windowHeight; y = y + sideLen) {//loop to create a grid in the y direction
     for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
       
